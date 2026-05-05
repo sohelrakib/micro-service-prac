867a72cc26c6688aad4fff024a98d3d112c594ff
Md. Sohel Akter <sohelrakib929@gmail.com>
2026-05-05 16:44:55 +0600

    rabbitmq direct

---
+++ product/src/app/Jobs/ProcessUserJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessUserJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        \Log::info("Message received:", $this->data);
+    }
+}
--- product/src/routes/web.php
+++ product/src/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Jobs\ProcessUserJob;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/rmq-send-direct', function () {
+    // php artisan queue:work rabbitmq --queue=direct_queue
+
+    ProcessUserJob::dispatch([
+        'name' => 'Sohel',
+        'type' => 'direct-test'
+    ]);
+
+    \Log::info("Message sent:", ['name' => 'Sohel', 'type' => 'direct-test']);
+    return "Message sent to RMQ";
 });
